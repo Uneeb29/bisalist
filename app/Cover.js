5b7fb84b69cd68a51d558a70689d149c1c9e7807
HunkHazard <jkabir.bscs21seecs@seecs.edu.pk>
2023-06-19 19:20:26 +0500

    navbar and cover done

---
+++ app/Cover.js
@@ -0,0 +1,194 @@
+import {
+  Stack,
+  Box,
+  Typography,
+  Link,
+  TextField,
+  InputAdornment,
+  Button,
+} from "@mui/material";
+import Navbar from "./Navbar";
+import SearchIcon from "@mui/icons-material/Search";
+
+import KeyboardAltOutlinedIcon from "@mui/icons-material/KeyboardAltOutlined";
+
+export default function Cover() {
+  return (
+    <Stack>
+      <Navbar></Navbar>
+
+      <Stack
+        sx={{
+          backgroundImage: 'url("landingImage.jpg")',
+          backgroundSize: "cover",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          zIndex: 0,
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            width: "100vw",
+            height: "100vh",
+            background: "rgba(0, 0, 0, 0.5)", // Adjust the alpha value to make it darker or lighter
+            zIndex: -1,
+          },
+        }}
+      >
+        <Typography
+          sx={{ fontSize: "55px", fontWeight: "bold", color: "white", mb: 1 }}
+        >
+          Fixing your problems one click at a time
+        </Typography>
+        <Typography sx={{ color: "white", fontSize: "14px", mb: 8 }}>
+          Find the right pro for every project with ease.
+        </Typography>
+        <Stack
+          direction={"row"}
+          sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}
+          width={"40%"}
+        >
+          <Link underline="none">
+            <Typography sx={{ color: "white" }}>Electrical</Typography>
+          </Link>
+          <Link underline="none">
+            <Typography sx={{ color: "white" }}>Plumber</Typography>
+          </Link>
+          <Link underline="none">
+            <Typography sx={{ color: "white" }}>Cleaning Services</Typography>
+          </Link>
+          <Link underline="none">
+            <Typography sx={{ color: "white" }}>Gypsum Works</Typography>
+          </Link>
+        </Stack>
+        <Box
+          sx={{
+            p: 0.75,
+            bgcolor: "rgba(255, 255, 255, 0.2)",
+            borderRadius: "8px",
+            width: "70%",
+            mb: 5,
+          }}
+        >
+          <TextField
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <KeyboardAltOutlinedIcon sx={{ color: "#4db4f9" }} />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Button
+                    sx={{
+                      bgcolor: "#334576",
+                      color: "white",
+                      p: 1,
+                      pl: 4,
+                      pr: 4,
+                    }}
+                  >
+                    <Typography
+                      sx={{ textTransform: "capitalize", fontSize: "14px" }}
+                    >
+                      Search
+                    </Typography>
+                    <SearchIcon
+                      sx={{ ml: 1, height: "20px", color: "#4db4f9" }}
+                    />
+                  </Button>
+                </InputAdornment>
+              ),
+            }}
+            placeholder="What are you looking for?"
+            sx={{
+              bgcolor: "white",
+              borderRadius: "8px",
+              width: "100%",
+              height: "full",
+            }}
+          ></TextField>
+        </Box>
+        <Stack direction={"row"} spacing={5}>
+          <Link underline="none" sx={{ cursor: "pointer" }}>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: "14px",
+                "&:hover": {
+                  color: "#4db4f9",
+                },
+              }}
+            >
+              Saloons
+            </Typography>
+          </Link>
+          <Link underline="none" sx={{ cursor: "pointer" }}>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: "14px",
+                "&:hover": {
+                  color: "#4db4f9",
+                },
+                fontWeight: "bold",
+              }}
+            >
+              Property Management
+            </Typography>
+          </Link>
+          <Link
+            underline="none"
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: "14px",
+                "&:hover": {
+                  color: "#4db4f9",
+                },
+                fontWeight: "bold",
+              }}
+            >
+              Plumber
+            </Typography>
+          </Link>
+          <Link underline="none" sx={{ cursor: "pointer" }}>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: "14px",
+                "&:hover": {
+                  color: "#4db4f9",
+                },
+                fontWeight: "bold",
+              }}
+            >
+              Masonry
+            </Typography>
+          </Link>
+          <Link underline="none" sx={{ cursor: "pointer" }}>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: "14px",
+                "&:hover": {
+                  color: "#4db4f9",
+                },
+                fontWeight: "bold",
+              }}
+            >
+              Home Services/Installation
+            </Typography>
+          </Link>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+}
---
+++ app/Navbar.js
@@ -0,0 +1,200 @@
+import {
+  Stack,
+  Box,
+  Divider,
+  Typography,
+  Link,
+  Badge,
+  Button,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import QueueOutlinedIcon from "@mui/icons-material/QueueOutlined";
+
+export default function Navbar() {
+  return (
+    <Stack
+      direction={"row"}
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        bgcolor: "#334576",
+        alignItems: "center",
+      }}
+      height={"5rem"}
+      width={"100%"}
+    >
+      <Box sx={{ ml: 4 }} height={"40%"}>
+        <img src="logo.png" style={{ height: "100%" }}></img>
+      </Box>
+      <Box
+        sx={{
+          borderRadius: "20px",
+          bgcolor: "#526394",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 1,
+        }}
+        width={"13%"}
+      >
+        <SearchIcon sx={{ color: "#4db4f9", mr: 1 }} />
+        <Divider
+          sx={{ bgcolor: "#4db4f9", height: "10px", width: "1px", mr: 1 }}
+        ></Divider>
+        <Typography
+          sx={{ color: "white", fontSize: "14px", fontWeight: "bold" }}
+        >
+          Search
+        </Typography>
+      </Box>
+      <Stack direction={"row"} spacing={3}>
+        <Link underline="none" sx={{ cursor: "pointer" }}>
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              "&:hover": {
+                color: "#4db4f9",
+              },
+              fontWeight: "bold",
+            }}
+          >
+            Home
+          </Typography>
+        </Link>
+        <Link underline="none" sx={{ cursor: "pointer" }}>
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              "&:hover": {
+                color: "#4db4f9",
+              },
+              fontWeight: "bold",
+            }}
+          >
+            Listings
+          </Typography>
+        </Link>
+        <Link
+          underline="none"
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              "&:hover": {
+                color: "#4db4f9",
+              },
+              fontWeight: "bold",
+            }}
+          >
+            Contact us
+          </Typography>
+        </Link>
+        <Link underline="none" sx={{ cursor: "pointer" }}>
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              "&:hover": {
+                color: "#4db4f9",
+              },
+              fontWeight: "bold",
+            }}
+          >
+            About us
+          </Typography>
+        </Link>
+        <Link underline="none" sx={{ cursor: "pointer" }}>
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              "&:hover": {
+                color: "#4db4f9",
+              },
+              fontWeight: "bold",
+            }}
+          >
+            Login
+          </Typography>
+        </Link>
+        <Link underline="none" sx={{ cursor: "pointer" }}>
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              "&:hover": {
+                color: "#4db4f9",
+              },
+              fontWeight: "bold",
+            }}
+          >
+            Track Jobs
+          </Typography>
+        </Link>
+      </Stack>
+      <Divider
+        sx={{ bgcolor: "#4db4f9", height: "20px", width: "1px", mr: 1 }}
+      ></Divider>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <PermIdentityIcon sx={{ color: "#4db4f9" }} />
+        <Typography
+          sx={{ color: "white", fontSize: "13px", fontWeight: "bold" }}
+        >
+          Sign in
+        </Typography>
+      </Box>
+      <Badge badgeContent={1} color="primary">
+        <FavoriteBorderIcon sx={{ color: "white" }} />
+      </Badge>
+      <Box
+        sx={{
+          p: 0.75,
+          bgcolor: "#526394",
+          borderRadius: "8px",
+          width: "13%",
+          mr: 1,
+        }}
+      >
+        <Button
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            bgcolor: "#4db4f9",
+            width: "100%",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "14px",
+              textTransform: "capitalize",
+              color: "white",
+              mr: 1,
+            }}
+          >
+            Add Listing
+          </Typography>
+          <Box>
+            <QueueOutlinedIcon sx={{ color: "white" }} />
+          </Box>
+        </Button>
+      </Box>
+    </Stack>
+  );
+}
