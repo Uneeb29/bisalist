d7a97b4b4d7b908dae8b4335d2e58f22db285ab5
HunkHazard <jkabir.bscs21seecs@seecs.edu.pk>
2023-06-23 20:24:08 +0500

    home page testimonials left only

---
+++ app/Listings.js
@@ -0,0 +1,57 @@
+import { Stack, Typography, Box, Divider } from "@mui/material";
+import ServiceCard from "./ServiceCard";
+
+export default function LandingPage() {
+  return (
+    <Stack direction="column" sx={{ mt: 7 }}>
+      <Box
+        sx={{
+          textAlign: "center",
+          display: "grid",
+          placeItems: "center",
+          position: "relative",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            fontSize: "60px",
+            fontWeight: "bold",
+            textTransform: "capitalize",
+            color: "rgba(0, 0, 0, 0.1)",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1",
+          }}
+        >
+          Fix it services
+        </Typography>
+        <Typography
+          variant="h2"
+          sx={{
+            color: "#607d8b",
+            fontSize: "30px",
+            fontWeight: "bold",
+            textTransform: "capitalize",
+          }}
+        >
+          The latest listings
+        </Typography>
+      </Box>
+      <Divider
+        sx={{
+          mt: 2,
+          width: "5rem",
+          bgcolor: "#4db4f9",
+          alignSelf: "center",
+          borderTopWidth: "2px",
+          borderRadius: "2px",
+          mb: 10,
+        }}
+      />
+      {/* <ServiceCard /> */}
+    </Stack>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -5,17 +5,26 @@ import LandingPage from "./LandingPage";
 import { CssBaseline, Stack } from "@mui/material";
 import ServiceCard from "./ServiceCard";
 import BecomeCustomer from "./BecomeCustomer";
+import Footer from "./Footer";
+import LandingPage from "./Listings";
+import PopularPlaces from "./PopularPlaces";
+import Packages from "./Packages";
 
 export default function Home() {
   return (
     <CssBaseline>
       <Stack direction={"column"}>
         <Navbar></Navbar>
+        <Cover></Cover>
+        <LandingPage></LandingPage>
+        <PopularPlaces></PopularPlaces>
+        <Packages></Packages>
+        <Footer></Footer>
         {/* <Navbar></Navbar>
         <Cover></Cover>
         <LandingPage></LandingPage>
         <ServiceCard></ServiceCard> */}
-        <BecomeCustomer></BecomeCustomer>
+        {/* <BecomeCustomer></BecomeCustomer> */}
       </Stack>
     </CssBaseline>
   );
