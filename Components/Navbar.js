87b37502ee40a2b987158ca8123fc09857e1c646
Muhammad Uneeb Bin Aftab <116509560+Uneeb29@users.noreply.github.com>
2023-06-26 11:07:08 +0500

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -208,9 +208,9 @@ export default function Navbar() {
         <Box
           sx={{
             position: "absolute",
-            top: "18%",
+            top: "15%",
             right: "0%",
-            transform: "translate(-50%, -50%)",
+            //transform: "translate(-50%, -50%)",
             width: 500,
             bgcolor: "background.paper",
             borderRadius: "8px",
