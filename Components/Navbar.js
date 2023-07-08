74d62600ccf2543675d61867b4aa218c784d777a
Muhammad Uneeb Bin Aftab <116509560+Uneeb29@users.noreply.github.com>
2023-07-08 19:06:11 +0500

    removed sign up from navbar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -15,12 +15,41 @@ import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import QueueOutlinedIcon from "@mui/icons-material/QueueOutlined";
 import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function Navbar() {
   const { data: session, status } = useSession();
+
+  if (session) {
+    console.log("session1", session);
+  }
+  const [anchorEl, setAnchorEl] = useState(null);
+  const menuOpen = Boolean(anchorEl);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSignOut = () => {
+    setAnchorEl(null);
+    signOut();
+  };
+
   return (
     <Stack
       direction="row"
@@ -41,29 +70,7 @@ export default function Navbar() {
       <Link sx={{ ml: 4, cursor: "pointer" }} height={"100%"} href="/">
         <img src="logo1.png" style={{ height: "50%", marginTop: "10%" }}></img>
       </Link>
-      <Box
-        sx={{
-          borderRadius: "20px",
-          bgcolor: "#526394",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          p: 1,
-          cursor: "pointer",
-        }}
-        width={"13%"}
-      >
-        <SearchIcon sx={{ color: "#4db4f9", mr: 1 }} />
-        <Divider
-          sx={{ bgcolor: "#4db4f9", height: "10px", width: "1px", mr: 1 }}
-        ></Divider>
-        <Typography
-          sx={{ color: "white", fontSize: "14px", fontWeight: "bold" }}
-        >
-          Search
-        </Typography>
-      </Box>
+
       <Stack direction={"row"} spacing={10}>
         <Link underline="none" sx={{ cursor: "pointer" }} href="/">
           <Typography
@@ -79,20 +86,7 @@ export default function Navbar() {
             Home
           </Typography>
         </Link>
-        {/* <Link underline="none" sx={{ cursor: "pointer" }}>
-          <Typography
-            sx={{
-              color: "white",
-              fontSize: "14px",
-              "&:hover": {
-                color: "#4db4f9",
-              },
-              fontWeight: "bold",
-            }}
-          >
-            Listings
-          </Typography>
-        </Link> */}
+
         <Link
           underline="none"
           sx={{
@@ -127,84 +121,142 @@ export default function Navbar() {
             About us
           </Typography>
         </Link>
-        {/* <Link underline="none" sx={{ cursor: "pointer" }}>
-          <Typography
-            sx={{
-              color: "white",
-              fontSize: "14px",
-              "&:hover": {
-                color: "#4db4f9",
-              },
-              fontWeight: "bold",
-            }}
-          >
-            Login
-          </Typography>
-        </Link> */}
-        {/* <Link underline="none" sx={{ cursor: "pointer" }}>
-          <Typography
-            sx={{
-              color: "white",
-              fontSize: "14px",
-              "&:hover": {
-                color: "#4db4f9",
-              },
-              fontWeight: "bold",
-            }}
-          >
-            Track Jobs
-          </Typography>
-        </Link> */}
       </Stack>
-      <Divider
-        sx={{ bgcolor: "#4db4f9", height: "20px", width: "1px", mr: 1 }}
-      ></Divider>
-      <Link underline="none" href="/login">
-        <Button
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            mr: 2,
-          }}
-        >
-          <PermIdentityIcon sx={{ color: "#4db4f9" }} />
-          <Typography
-            sx={{
-              color: "white",
-              fontSize: "13px",
-              fontWeight: "bold",
-              textTransform: "capitalize",
-            }}
-          >
-            Sign in
-          </Typography>
-        </Button>
-      </Link>
-      <Button
+
+      <Box
         sx={{
+          width: "20%",
           display: "flex",
-          flexDirection: "row",
           justifyContent: "space-between",
           alignItems: "center",
-          mr: 2,
         }}
-        onClick={handleOpen}
       >
-        <PermIdentityIcon sx={{ color: "#4db4f9" }} />
-        <Typography
-          sx={{
-            color: "white",
-            fontSize: "13px",
-            fontWeight: "bold",
-            textTransform: "capitalize",
-          }}
-        >
-          Sign up
-        </Typography>
-      </Button>
-      <Modal
+        <Divider
+          sx={{ bgcolor: "#4db4f9", height: "20px", width: "1px", mr: 1 }}
+        ></Divider>
+
+        {session && (
+          <Box>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ mr: 8 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar sx={{ width: 38, height: 38 }}>
+                  {session.user.name.charAt(0).toUpperCase()}
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={menuOpen}
+              onClose={handleCloseMenu}
+              onClick={handleCloseMenu}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem>
+                <Avatar /> My account
+              </MenuItem>
+              <Divider />
+
+              <MenuItem onClick={handleSignOut}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Sign Out
+              </MenuItem>
+            </Menu>
+          </Box>
+        )}
+
+        {!session && (
+          // <div>
+          <Link underline="none" href="/login">
+            <Button
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                mr: 2,
+              }}
+            >
+              <PermIdentityIcon sx={{ color: "#4db4f9" }} />
+              <Typography
+                sx={{
+                  color: "white",
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                }}
+              >
+                Sign in
+              </Typography>
+            </Button>
+          </Link>
+        )}
+        {
+          // !session && (
+          //   <Button
+          //     sx={{
+          //       display: "flex",
+          //       flexDirection: "row",
+          //       justifyContent: "space-between",
+          //       alignItems: "center",
+          //       mr: 2,
+          //     }}
+          //     onClick={handleOpen}
+          //   >
+          //     <PermIdentityIcon sx={{ color: "#4db4f9" }} />
+          //     <Typography
+          //       sx={{
+          //         color: "white",
+          //         fontSize: "13px",
+          //         fontWeight: "bold",
+          //         textTransform: "capitalize",
+          //       }}
+          //     >
+          //       Sign up
+          //     </Typography>
+          //   </Button>
+          // )
+          // // </div>
+        }
+      </Box>
+
+      {/* <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
@@ -257,7 +309,7 @@ export default function Navbar() {
             <Typography>Become a Vendor</Typography>
           </Link>
         </Box>
-      </Modal>
+      </Modal> */}
 
       {/* <Badge badgeContent={1} color="primary">
         <FavoriteBorderIcon sx={{ color: "white" }} />
